d7c21171f568801fd644c4ead65896eff8efb572
BorTry <trym.w.borg@gmail.com>
2024-11-23 17:14:03 +0100

    Fixed edge case where menu got hidden after resize on git page

--- scripts/menu-button.js
+++ scripts/menu-button.js
@@ -1,7 +1,13 @@
-var buttonState = true
+var buttonState = false
 
 // Flips the visibility for the drop down menu
 function dropDownButtton() {
-    document.getElementById("navigation-menu").style.visibility = buttonState ? "visible" : "hidden" ;
     buttonState = !buttonState
+    document.getElementById("navigation-menu").style.visibility = buttonState ? "visible" : "hidden" ;
 }
+
+// Edge case for when the button has been pressed, but the screen gets resized after
+window.addEventListener("resize", function() {
+    document.getElementById("navigation-menu").style.visibility = this.innerWidth > 750 ? "visible" : "hidden" ;
+    buttonState = this.innerWidth > 750
+})
